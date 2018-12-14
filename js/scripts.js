92dd1f8a2c7f3e0cac7705c43bdfa601353af181
blackrose <wahomekelving@gmail.com>
2018-12-14 18:47:14 +0300

    added roll,hold and winner functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,33 @@ var rollDice = function (){
 };
 
 function Player (){
-  
-}
+  this.roll = 0;
+  this.playerName = 0;  //player constructor
+  this.score= 0 ;
+  this.totalScore = 0;
+
+};
+ // Roll Hold and Winner
+Player.prototype.rolll = function(){
+  if (this.roll === 1){
+    this.score = 0;
+    alert("You rolled a 1!Next player's turn!"); //Roll
+  }
+  else{
+    this.score += this.roll;
+  }
+};
+
+
+
+
+Player.prototype.hold = function(){
+  this.totalScore += this.score;    //Hold
+  this.score = 0;
+};
+
+Player.prototype.winner = function(){
+  if (this.totalScore >= 100){                         //winner
+    alert(this.playerName + "has won the game!!");
+  }
+};
